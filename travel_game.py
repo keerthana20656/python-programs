1df0c788779ca1078359b62df077101de1d29174
keerthana <60438094+keerthana20656@users.noreply.github.com>
2020-02-16 22:51:35 -0800

    Add files via upload

---
+++ travel_game.py
@@ -0,0 +1,20 @@
+while(1):
+    print("enter choice (1,2,3,4,0)")
+    x=int(input())
+    if(x==1):
+        print("Go to college")
+        continue
+    elif(x==2):
+        print("Go on vacation/movie")
+        continue
+    elif(x==3):
+        print("Take rest")
+        continue
+    elif(x==4):
+        print("Play games")
+        continue
+    elif(x==0):
+        print("quit")
+        break
+    else:
+        continue
